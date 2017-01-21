8d738d1ce7b0a251b81a979ec5e2cc1acb70305b
Tim Flynn <MC42@users.noreply.github.com>
2017-01-20 22:43:08 -0500

    Add files via upload

---
+++ ugly.php
@@ -0,0 +1,51 @@
+<html>
+ <head>
+  <title><?php 
+  $district = $_GET['district'];
+  $district = strtoupper($district);
+  echo $district . ' Rankings | The Ugly Alliance';
+  ?></title>
+  
+  <style>
+  
+  body {
+	  font-family: Arial;
+  }
+  h1,h2,h3.h4,h5,h6 {
+	  text-align:center;
+  }
+  .center {
+width:420px; 
+margin:0 auto;
+  }
+  .table {
+	  width: 100%;
+	  text-align:center;
+  }
+  </style>
+ </head>
+ <body>
+ <?php 
+ include $_SERVER['DOCUMENT_ROOT'] . '/tbaapi/TBARequest.php';
+ $tbaRequest = new tbaAPI\TBARequest("@StruckByTheBell", "UglyAlliance-Rankings", "1");
+
+if (isset($_GET['district'])) { //THIS IS ALL A MESS WATCH WITH CAUTION
+	
+	$district = $_GET['district'];
+	echo '<div class="center"><h2>' . strtoupper($district) . ' District Rankings</h2>';
+	$distRank = $tbaRequest->getDistrictRankings(['district_short'=>$district, 'year'=>date("Y")]);
+	$nonJSDistRank = json_decode($distRank);
+	$rank = "1";
+	
+	echo '<table class="table" border="0"><tr><th>Rank</th><th>Number</th><th>Points</th></tr>';
+	foreach($nonJSDistRank as $key) {
+	echo '<tr><td>' . $rank . '</td><td>' . strtoupper($key->team_key) . "</td><td>" . $key->point_total . "</td></tr>";
+	$rank++;
+	} 
+	echo '</table></div>';
+}
+
+echo '<h6> Generated on ' . date("d-m-Y h:i:s"). ' with The Ugly Alliance.</h6>';
+ ?> 
+ </body>
+</html>
